8acc4ec1360a6067bd9c5b5d37e7100daa9481b9
Leonardo Delgado <sdelgadom@unal.edu.co>
2021-04-11 11:55:50 -0500

    escritura y lectura de binarios

--- uber_movement_practice/search_process.c
+++ uber_movement_practice/search_process.c
@@ -4,6 +4,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 int main(){
-	
+	FILE *practice;
+	practice = fopen("practicabinaria.dat", "w+");
+	int val[5] = {1, 2, 3, 5, 9938};
+	fwrite(val, sizeof(int), 5, practice);
+	fclose(practice);
+	FILE *lectura;
+	lectura = fopen("practicabinaria.dat", "r");
+	int buffer[5];
+	fread(buffer, sizeof(int), 5, lectura);
+	for(int i = 0; i < 5; i++){
+		printf("%d\n", buffer[i]);	
+	}
+	fclose(lectura);
 	return 0;
 }

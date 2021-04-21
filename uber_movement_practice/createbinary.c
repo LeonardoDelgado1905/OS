84627d5518378613705d788a0113bfabaeeec99b
Leonardo Delgado <sdelgadom@unal.edu.co>
2021-04-20 21:52:29 -0500

    total limpio

--- uber_movement_practice/createbinary.c
+++ uber_movement_practice/createbinary.c
@@ -7,31 +7,32 @@
 #include <stdbool.h>
 #include "find_library.h"
 
+
 int *heads;
 int *tails;
 int main(){
-
-	NUM_ORIGINS = 1160;
+	NUM_ORIGINS = 1160;		// Inicializa un puntero al valor donde inicia el arreglo de heads
+	heads = malloc(NUM_ORIGINS*sizeof(int)); 	// Libera el espacio para guardar n estucturas router	
 	tails = malloc(NUM_ORIGINS*sizeof(int));
+
 	memset(heads, -1, NUM_ORIGINS*sizeof(int));
 	memset(tails, -1, NUM_ORIGINS*sizeof(int));
 	
 	binaryFile = fopen("linkedlist.bin", "wb");
 	binaryFileR = fopen("linkedlist.bin", "rb");
 	
-
+	FILE *cadastral_data;
 	cadastral_data = fopen("bogota-cadastral.csv", "r");
 	
 	struct router myrouter;
 	
 	char line[1024];
 	fgets(line, 1024, cadastral_data);
-
 	int m= 0;
 	int cota = 100000;
-
     while (fgets(line, 1024, cadastral_data))//Lee cada csv de el archivo
     {	
+		
 		char* tmp = strdup(line);
 		char* sourceid = getfield(tmp, 0);
 		memcpy(myrouter.sourceid, sourceid, sizeof(sourceid));
@@ -44,16 +45,20 @@ int main(){
 		myrouter.mean_travel_time = atof(getfield(tmp, 3));
 		tmp = strdup(line);
 		myrouter.standard_deviation_travel_time = atof(getfield(tmp, 4));
+		if((m % 1000000) == 0){
+			printf("Datos Cargados: +%d\n", m);
+		}
 	 	add(heads, tails, m++, myrouter);		
         free(tmp);
     }
-
+	printf("Total datos Cargados: +%d\n", m);
 	FILE *hash_table;
 	hash_table = fopen("hash_table.bin", "wb+");
 
-	fwrite(heads,sizeof(int),n,hash_table);
+	fwrite(heads,sizeof(int),NUM_ORIGINS,hash_table);
 	free(heads);
 	free(tails);
+
 	fclose(binaryFile);
 	fclose(binaryFileR);
 	fclose(hash_table);
@@ -61,3 +66,4 @@ int main(){
 
 	return 0;
 }
+
